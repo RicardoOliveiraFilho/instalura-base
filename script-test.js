bf453ec30a3885517e2a1bcd911152260cc3823b
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2021-03-06 00:48:55 -0300

    Commit Maneiro

--- script-test.js
+++ script-test.js
@@ -2,4 +2,6 @@ const shell = require('shelljs');
 
 console.log('Olá Mundo');
 
-shell.exec('git diff --name-only branch-scripts-testes..main');
+const resultado = shell.exec('git diff --name-only branch-scripts-testes..main', { silent: true });
+
+console.log(resultado.stdout.split('\n'));
