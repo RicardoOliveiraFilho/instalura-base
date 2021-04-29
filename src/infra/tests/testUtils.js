3f216603291ac9ca916cfdcb02d1f2e4551d4998
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2021-04-29 15:49:10 -0300

    feat(teste em componentes): testando o componente TextField com o Jest

---
+++ src/infra/tests/testUtils.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { render } from '@testing-library/react';
+
+import WebsiteGlobalProvider from '../../components/wrappers/WebsitePage/provider';
+
+const AllTheProviders = ({ children, ...props }) => (
+  <WebsiteGlobalProvider {...props}>
+    {children}
+  </WebsiteGlobalProvider>
+);
+
+AllTheProviders.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const customRender = (ui, options = {}) => {
+  const Provider = (props) => <AllTheProviders {...props} {...options.providerProps} />;
+
+  return render(ui, { wrapper: Provider, ...options });
+};
+
+export * from '@testing-library/react';
+
+export { customRender as render };
