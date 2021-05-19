d181de8f9a6bc050a4dbbca13a16fea335cebe0b
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2021-05-19 14:19:08 -0300

    feat(pagina sobre): utilizaçao do GraphQL para realizar as consultas no DatoCMS para obter dados

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,8 +1,42 @@
 import React from 'react';
+import { GraphQLClient, gql } from 'graphql-request';
 import AboutScreen from '../src/components/screens/AboutScreen';
 import WebsitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
-export default WebsitePageHOC(AboutScreen, {
+function AboutPage({ messages }) {
+  return <AboutScreen messages={messages} />;
+}
+
+const TOKEN = '8c06f5d31704ed49911c013a5054a5';
+
+export async function getStaticProps() {
+  const DatoCMSURL = 'https://graphql.datocms.com/';
+
+  const client = new GraphQLClient(DatoCMSURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    query {
+      pagesobre {
+        pageTitle
+        pageDescription
+      }
+    }
+  `;
+
+  const messages = await client.request(query);
+
+  return {
+    props: {
+      messages,
+    },
+  };
+}
+
+export default WebsitePageHOC(AboutPage, {
   pageWrapperProps: {
     seoProps: {
       headTitle: 'Sobre',
