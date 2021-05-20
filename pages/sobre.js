c0e5b59c576cefc2110696df6feeb825e0bda92a
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2021-05-20 14:36:43 -0300

    refactor(cmsgraphqlcliente): desaclopamos a consulta com GraphQL e a integraçao com o DatoCMS

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,33 +1,13 @@
 import React from 'react';
-import { GraphQLClient, gql } from 'graphql-request';
-import AboutScreen from '../src/components/screens/AboutScreen';
+import AboutScreen, { getContent } from '../src/components/screens/AboutScreen';
 import WebsitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
 function AboutPage({ messages }) {
   return <AboutScreen messages={messages} />;
 }
 
-const TOKEN = process.env.DATO_CMS_TOKEN;
-
 export async function getStaticProps() {
-  const DatoCMSURL = 'https://graphql.datocms.com/';
-
-  const client = new GraphQLClient(DatoCMSURL, {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  });
-
-  const query = gql`
-    query {
-      pagesobre {
-        pageTitle
-        pageDescription
-      }
-    }
-  `;
-
-  const messages = await client.request(query);
+  const messages = await getContent();
 
   return {
     props: {
