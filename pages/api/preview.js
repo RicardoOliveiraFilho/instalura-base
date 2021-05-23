8a4b175b7c32bb5abc415cf09cbdbd2f4b6a21f5
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2021-05-23 16:07:16 -0300

    refactor(fix linter): adiçao da correçao sugerida pelo Linter de codigo

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
 
   const key = 'BIRDMAN';
 
-  if(req.query.key !== key) {
+  if (req.query.key !== key) {
     return res.status(401).json({ message: 'Invalid key to enable preview' });
   }
 
